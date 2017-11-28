adfcc6bc005567ed53facb6d91af37b245f71bda
Max Hutchinson <maxhutch@gmail.com>
2017-11-28 07:12:59 -0600

    Bound the citrination client version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='pif_ingestor',
       install_requires=[
           "pypif>=2.0.0,<3",
           "pypif_sdk>=2.0.0,<3",
-          "citrination_client",
+          "citrination_client>=2,<4",
           "stevedore"
       ],
       extra_require={
