3ba9738cc6cf8a05098b33f6ef1f288efded2a10
Max Gallant <mcgallant72@gmail.com>
2018-10-23 15:21:26 -0700

    Bump pypif sdk version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='pif_ingestor',
       },
       install_requires=[
           "pypif>=2.0.0,<3",
-          "pypif_sdk>=2.0.0,<3",
+          "pypif_sdk>=2.2.1,<3",
           "citrination_client>=4.4.1,<5",
           "stevedore"
       ],
